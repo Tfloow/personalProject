7af40e7dc87eaf50c14591bd97123041d7c5b054
Tfloow <88899420+Tfloow@users.noreply.github.com>
2021-09-11 10:30:10 +0200

    Add files via upload

---
+++ basicWebscraping.py
@@ -0,0 +1,12 @@
+from urllib.request import urlopen
+from time import time, sleep
+
+
+while True:
+    sleep(600 - time() % 600)
+    url = "URL"
+    page = urlopen(url)
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+    Prim_index = html.find("YourSearch")
+    print(Prim_index)
